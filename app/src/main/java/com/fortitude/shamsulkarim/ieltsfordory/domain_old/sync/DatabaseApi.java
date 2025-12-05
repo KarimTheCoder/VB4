db0bf0049d0057808a7f11cd33c62be2666c6c8d
Shamsul1 <Banglish123@gmail.com>
2025-12-05 09:47:29 +0600

    5th 9:47am

--- app/src/main/java/com/fortitude/shamsulkarim/ieltsfordory/domain_old/sync/DatabaseApi.java
+++
@@ -1,8 +0,0 @@
-package com.fortitude.shamsulkarim.ieltsfordory.domain_old.sync;
-
-import com.fortitude.shamsulkarim.ieltsfordory.data_old.FavLearnedState;
-
-public interface DatabaseApi {
-    void setValue(String userId, FavLearnedState state);
-}
-
--- app/src/main/java/com/fortitude/shamsulkarim/ieltsfordory/domain_old/sync/NetworkChecker.java
+++
@@ -1,6 +0,0 @@
-package com.fortitude.shamsulkarim.ieltsfordory.domain_old.sync;
-
-public interface NetworkChecker {
-    boolean isConnected();
-}
-
